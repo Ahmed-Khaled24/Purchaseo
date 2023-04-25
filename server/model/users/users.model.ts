751273526ff84b08087315a515a8d6a3ba16a50e
Omar Fahmy <Ofahmy1234@gmail.com>
2023-04-25 15:09:02 +0200

    feat: add user model functions with mysql2

---
+++ server/model/users/users.model.ts
@@ -0,0 +1,61 @@
+
+import { RowDataPacket } from "mysql2";
+import { mysqlConnection } from "../../service/mysql2";
+
+async function getUserById(id: string) : Promise<RowDataPacket> {
+    const [rows, fields] = await mysqlConnection.execute("SELECT * FROM user WHERE user_id = ?", [id]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows[0];
+};
+
+async function getUserByEmail(email: string) : Promise<RowDataPacket> {
+    const [rows, fields] = await mysqlConnection.execute("SELECT * FROM user WHERE email = ?", [email]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows[0];
+}
+
+async function getUsersByName(name: string) : Promise<RowDataPacket[]> {
+    const [rows, fields] = await mysqlConnection.execute("SELECT * FROM user WHERE name = ?", [name]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows;
+}
+
+// Change user type when we establish the user model
+async function addUser( {name, email, password}:{name: string, email: string, password: string}) : Promise<boolean> {
+    const [rows, fields] = await mysqlConnection.execute("INSERT INTO user (name, email, password) VALUES (?, ?, ?)", [name, email, password]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows? true : false;
+}
+
+// waiting for the user model to know what to update
+async function updateUserByEmail({email, update}:{email: string ,update: any}) : Promise<boolean> {
+    let updateParams = Object.keys(update).map((key) => {
+        return `${key} = ?`
+    });
+    let query = `UPDATE user SET ${updateParams.join(", ")} WHERE email = ?` ;
+    console.log([...Object.values(update),email])
+    const [rows, fields] = await mysqlConnection.execute(query, [...Object.values(update),email]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows? true : false;
+}
+
+async function deleteUserById(id: number) : Promise<boolean> {
+    const [rows, fields] = await mysqlConnection.execute("DELETE FROM user WHERE id = ?", [id]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows? true : false;
+}
+
+async function deleteUserByEmail(email: string) : Promise<boolean> {
+    const [rows, fields] = await mysqlConnection.execute("DELETE FROM user WHERE email = ?", [email]);
+    const ResultRows = rows as RowDataPacket[];
+    return ResultRows? true : false;
+}
+
+export { getUserById,
+         getUserByEmail,
+         getUsersByName,
+         addUser,
+         deleteUserById,
+         deleteUserByEmail,
+         updateUserByEmail 
+        };

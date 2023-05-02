f32e0b699ba8a5f9c53bd9fd2db94ad56da8d855
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-02 05:04:14 +0300

    feat: update the get user routes - add the get user by id route - add the get user by email route - add regex for the id route params - add regex for the email route params

--- server/Routers/user.router.ts
+++ server/Routers/user.router.ts
@@ -1,14 +1,19 @@
-import {Router, Request, Response} from 'express';
-import {addNewUser, getUser, removeUser, updateUser} from '../controller/user.controller';
+import { Router, Request, Response } from "express";
+import {
+    addNewUser,
+    removeUser,
+    updateUser,
+    getUserByEmail,
+    getUserById,
+} from "../controller/user.controller";
 
 const userRouter = Router();
 
-
-userRouter.get('/:id', getUser);
-userRouter.route('/')
-.post(addNewUser)
-.delete(removeUser)
-.patch(updateUser)
-
+userRouter.get("/id/:id([0-9]+)", getUserById);
+userRouter.get(
+    "/email/:email([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,})",
+    getUserByEmail
+);
+userRouter.route("/").post(addNewUser).delete(removeUser).patch(updateUser);
 
 export default userRouter;

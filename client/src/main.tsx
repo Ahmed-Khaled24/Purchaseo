45709c281ebac03d8b3f76e121176d2bdf691b7e
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-05-04 03:42:47 +0300

    Added the cartReducer to the main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App.jsx'
 import './index.css'
+import CarttReducer from './features/cartSlice'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+const store=configureStore({
+  reducer:{
+    cart:CarttReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
+  
   </React.StrictMode>,
 )

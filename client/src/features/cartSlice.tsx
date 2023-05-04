7dab8b9880942951f8b8fb3d7bddc799f6f458e1
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-05-04 03:41:09 +0300

    feat: Add cart functionality to cartSlice using Redux Toolkit. This commit adds cart functionality to the cartSlice using the createSlice function provided by the @reduxjs/toolkit package. The cartSlice now includes actions to add, decrease, and remove items from the cart, calculate the total amount of the cart, and clear the cart.

---
+++ client/src/features/cartSlice.tsx
@@ -0,0 +1,87 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState={
+    cartItems:[],
+    cartTotalQuantity:0,
+    cartTotalAmount:0,
+};
+const cartSlice=createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addToCart(state,action){
+            const existingIndex = state.cartItems.findIndex(
+                (item) => item.id === action.payload.id
+              );
+            if (existingIndex >= 0) {
+                state.cartItems[existingIndex] = {
+                  ...state.cartItems[existingIndex],
+                  cartQuantity: state.cartItems[existingIndex].cartQuantity + 1,
+                };} 
+                else {
+                let tempProductItem = { ...action.payload, cartQuantity: 1 };
+                state.cartItems.push(tempProductItem);
+              }
+      
+        },
+        decreaseCart(state, action) {
+            const itemIndex = state.cartItems.findIndex(
+              (item) => item.id === action.payload.id
+            );
+      
+            if (state.cartItems[itemIndex].cartQuantity > 1) {
+              state.cartItems[itemIndex].cartQuantity -= 1;
+
+            } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+              const nextCartItems = state.cartItems.filter(
+                (item) => item.id !== action.payload.id
+              );
+      
+              state.cartItems = nextCartItems;
+            }
+      
+          },
+          removeFromCart(state, action) {
+            state.cartItems.map((cartItem) => {
+              if (cartItem.id === action.payload.id) {
+                const nextCartItems = state.cartItems.filter(
+                  (item) => item.id !== cartItem.id
+                );
+      
+                state.cartItems = nextCartItems;
+
+              }
+       
+            });
+          },
+          getTotals(state) {
+            let { total, quantity } = state.cartItems.reduce(
+              (cartTotal, cartItem) => {
+                const { price, cartQuantity } = cartItem;
+                const itemTotal = price * cartQuantity;
+      
+                cartTotal.total += itemTotal;
+                cartTotal.quantity += cartQuantity;
+      
+                return cartTotal;
+              },
+              {
+                total: 0,
+                quantity: 0,
+              }
+            );      
+            total = Math.round(total);
+            state.cartTotalQuantity = quantity;
+            state.cartTotalAmount = total;
+          },
+          clearCart(state) {
+            state.cartItems = [];
+          },
+
+    },
+});
+export const { addToCart, decreaseCart, removeFromCart, getTotals, clearCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;

7761c70e3f832c824b15761ec11f955861143344
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-11 13:27:56 +0300

    feat: update productValidation
    handle more edge cases

--- server/util/validation/productValidation.ts
+++ server/util/validation/productValidation.ts
@@ -37,16 +37,27 @@ export function validateNumber(
 	fieldName: string
 ) {
 	let isValid =
-		typeof theNumber === 'string' &&
-		theNumber.match(/\d+/g) &&
-		Number(theNumber) >= 0;
+		(typeof theNumber === 'string' && // string but only numbers
+			theNumber.match(/\d+/g) &&
+			Number(theNumber) >= 0) ||
+		(typeof theNumber === 'number' && theNumber >= 0);
+	console.log(`${theNumber} is valid: ${isValid}`);
 	if (required && !theNumber) {
+		// required but not provided
 		throw new ErrorWithStatusCode(
 			`mandatory field "${fieldName}" is not provided in the product object`,
 			400
 		);
 	}
 	if (required && !isValid) {
+		// required and provided but invalid
+		throw new ErrorWithStatusCode(
+			`${fieldName} is invalid, it must be a number greater than or equal to zero`,
+			400
+		);
+	}
+	if (!required && theNumber && !isValid) {
+		// not required but provided and invalid
 		throw new ErrorWithStatusCode(
 			`${fieldName} is invalid, it must be a number greater than or equal to zero`,
 			400
@@ -59,14 +70,26 @@ export function validateString(
 	required: boolean,
 	fieldName: string
 ) {
+	if (theString === '') {
+		theString = ' ';
+	}
 	let isValid = typeof theString === 'string' && theString.trim().length >= 1;
 	if (required && !theString) {
+		// required but not provided
 		throw new ErrorWithStatusCode(
 			`mandatory field "${fieldName}" is not provided in the product object`,
 			400
 		);
 	}
 	if (required && !isValid) {
+		// required and provided but invalid
+		throw new ErrorWithStatusCode(
+			`${fieldName} is invalid, it must be a non empty string`,
+			400
+		);
+	}
+	if (!required && theString && !isValid) {
+		// not required but provided and invalid
 		throw new ErrorWithStatusCode(
 			`${fieldName} is invalid, it must be a non empty string`,
 			400

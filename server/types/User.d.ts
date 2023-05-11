379013fe67710e5cfb4f7b6af1f4ae950937cf19
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-11 05:38:08 +0300

    feat: add subtypes in user type

--- server/types/User.d.ts
+++ server/types/User.d.ts
@@ -4,18 +4,23 @@ export interface Timestamps {
     createdAt?: Timestamp,
     updatedAt?: Timestamp
 }
+
+export type Role = 'Seller' | 'Customer' | 'Admin' | 'Company'
+export type social_type = 'google' | 'local'
+export type user_type = 'Local' | 'Both' | 'Non-Local'
 export interface User extends Timestamps {
     user_id?: number|string,
     image_url?: string,
     // TODO: in validation check Fname and Lname
     Fname: string,
     Lname: string,
+    phone_number?: string,
     email: string,
-    role: 'Seller' | 'Customer' | 'Admin' | 'Company',
+    role: Role,
     password: string,
     social_id?: string,
-    social_type?: 'google' | 'local',
-    user_type?: 'Local' | 'Both' | 'Non-Local',
+    social_type?: social_type,
+    user_type?: user_type,
     birthDate?: Date,
 }
 
@@ -23,3 +28,4 @@ export interface Admin extends User {
     admin_id?: number|string
     permission?: "adminstrator"
 }
+

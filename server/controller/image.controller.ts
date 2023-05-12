87734eec0e998aad4ccc2e5c9e99f3e26d8aa1bf
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-13 00:37:04 +0300

    feat: add the add user image url controller

--- server/controller/image.controller.ts
+++ server/controller/image.controller.ts
@@ -1,11 +1,50 @@
 import { generateUploadUrl } from "../services/s3";
 import ErrorWithStatusCode from "../util/classes/ErrorWithStatusCode";
 import { Request, Response } from "express";
+import crypto from "crypto";
+import { promisify } from "util";
+import { dbUpdateUserById } from "../model/users.model";
+
+const randomBytes = promisify(crypto.randomBytes);
 
 export async function getUploadUrl(req: Request, res: Response) {
     try {
-        const url = await generateUploadUrl();
+        console.log("getUploadUrl");
+        const imageName = (await randomBytes(16)).toString("hex");
+        const directoryName = req.user?.user_id as number;
+        const imageUrl = `Users/User_${directoryName}/${imageName}`;
+        const url = await generateUploadUrl(imageUrl);
         res.status(200).json({ status: "success", data: url });
+    } catch (error: ErrorWithStatusCode | any) {
+        res.status(error.statusCode || 500).json({
+            status: "failure",
+            message: error.message,
+        });
+    }
+}
+
+export async function addUserImage(req: Request, res: Response) {
+    try {
+        const { imageUrl } = req.body;
+        console.log({ imageUrl });
+        const userId = req.user?.user_id as number;
+        await dbUpdateUserById(userId, { image_url: imageUrl });
+        return res.status(200).json({ status: "success", data: "image added" });
+    } catch (error: ErrorWithStatusCode | any) {
+        res.status(error.statusCode || 500).json({
+            status: "failure",
+            message: error.message,
+        });
+    }
+}
+
+export async function addProductImage(req: Request, res: Response) {
+    try {
+        const { imageUrl } = req.body;
+        console.log({ imageUrl });
+        const userId = req.user?.user_id as number;
+        await dbUpdateUserById(userId, { image_url: imageUrl });
+        return res.status(200).json({ status: "success", data: "image added" });
     } catch (error: ErrorWithStatusCode | any) {
         res.status(error.statusCode || 500).json({
             status: "failure",

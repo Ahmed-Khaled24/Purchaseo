f30759fd79d436547b1e2deb16a22b61849ae2cb
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-13 00:35:48 +0300

    fix: put command in signed url

--- server/services/s3.ts
+++ server/services/s3.ts
@@ -1,11 +1,8 @@
-import { GetObjectCommand, S3, S3ClientConfig } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3, S3ClientConfig } from "@aws-sdk/client-s3";
 import keys from "../config/keys";
-import crypto from "crypto";
-import { promisify } from "util";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import ErrorWithStatusCode from "../util/classes/ErrorWithStatusCode";
 
-const randomBytes = promisify(crypto.randomBytes);
 
 // TODO: Change the CORS in the S# BUcket for production
 
@@ -23,21 +20,32 @@ const s3 = new S3({
     },
 });
 
-export async function generateUploadUrl() {
+export async function generateUploadUrl(imageName: string) {
     // image name should be unique may be passed from client side
     try {
-        const rawBytes = await randomBytes(16);
-        const imageName = rawBytes.toString("hex");
-
-        const command = new GetObjectCommand({
+        const command = new PutObjectCommand({
             Bucket: bucketName,
             Key: imageName,
         });
 
-        const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 60 });
+        const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 120 });
         console.log({ uploadUrl });
         return uploadUrl;
     } catch (error: ErrorWithStatusCode | any) {
         throw new ErrorWithStatusCode(error.message, 500);
     }
 }
+
+export async function deleteImageFromS3Bucket(key: string) {
+    // Todo update the iam user policy to delete object
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+    };
+    try {
+      const data = await s3.send(new DeleteObjectCommand(params));
+      console.log(`Successfully deleted ${key} from ${bucketName}`);
+    } catch (err) {
+      console.log("Error", err);
+    }
+  }

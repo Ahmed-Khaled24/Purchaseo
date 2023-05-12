a4c9b461106edd3b6ed0790a61452e355893da22
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-12 09:35:13 +0300

    feat: add the get signed url endpoint to /image

--- server/Routers/index.ts
+++ server/Routers/index.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import userRouter from './user.router';
 import productRouter from './products.router';
 import authRouter from './auth.router';
+import imageRouter from './image.router';
 
 const globalRouter = Router();
 
 globalRouter.use('/user', userRouter);
 globalRouter.use('/product', productRouter)
 globalRouter.use('/auth', authRouter)
+globalRouter.use('/image', imageRouter)
 
 export default globalRouter;

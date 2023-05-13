6b682e516a938fb3b68e073f8c999eb894d8e276
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-14 00:16:10 +0300

    feat: add whitelist middleware

--- server/Routers/index.ts
+++ server/Routers/index.ts
@@ -3,12 +3,17 @@ import userRouter from './user.router';
 import productRouter from './products.router';
 import authRouter from './auth.router';
 import imageRouter from './image.router';
+import { mwCheckLoginStatus } from '../middlewares/auth/user.middleware';
 
 const globalRouter = Router();
 
 globalRouter.use('/user', userRouter);
 globalRouter.use('/product', productRouter)
 globalRouter.use('/auth', authRouter)
-globalRouter.use('/image', imageRouter)
+globalRouter.use('/image',mwCheckLoginStatus("LoggedIn"), imageRouter)
+// TODO: make this in front end cuz we put react here
+globalRouter.get('/*', (req, res) => {
+    res.status(404).json({status: 'failure', data: 'path not found'})
+})
 
 export default globalRouter;

de7b2e44f0a4e73ecae955fb362019a9e47f6fc7
Abd-El-Rahman <abdonasser865@gmail.com>
2023-05-15 16:43:00 +0300

    changed branch name

--- server/middlewares/security/cors.middleware.ts
+++ server/middlewares/security/cors.middleware.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 export const corsMiddleware = cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://localhost:3000",
     credentials: true,
     optionsSuccessStatus: 200,
 });

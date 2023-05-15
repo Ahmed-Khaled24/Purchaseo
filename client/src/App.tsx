89bc9279f15e8b649dca74396b165ec96ae85a69
Abd-El-Rahman <abdonasser865@gmail.com>
2023-05-15 18:15:36 +0300

    Routing of the seller done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './app.css';
 import React, { useState, useEffect } from "react"
 import Home from "./components/Home"
 import NavBar from "./components/NavBarNormal"
+import NavBar2 from "./components/NavBarSeller"
 import Products from "./components/Products"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Item from './components/Item';
@@ -9,7 +10,7 @@ import Cart from './components/Cart';
 import AddReview from './components/Add-Review'
 import Footer from './components/Footer'
 import Reviews from './components/Reviews'
-import OwnerProduct from './components/Owner-Product'
+import OwnerItems from './components/OwnerItems'
 import UserPage from './components/UserPage';
 import Container from './components/Container'
 import CreateAccountContainer from './components/CreateAccountContainer'
@@ -22,21 +23,20 @@ function App() {
   const items = ["item1"];
   const desc1 = "Lorem ipsum dolor sit amet, aute irure ";
   const desc2 = "Lorem ipsum dolor sit amet, aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-
-  const navbar = () => {
-
-  }
-
+  let userType = false;
   return (
 
     <div className="App">
 
       <Router>
-        <NavBar />
+        <NavBar type={userType}/>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="/seller-products" element={<OwnerProduct name="Item 1" rate={3} reviews={50} desc={desc2} />} />
+            <Route path='/seller'>
+              <Route index element={<OwnerItems />} />
+              <Route path="add-product" element={<AddProduct />} />
+            </Route>
             <Route path="/products/:category" element={<Products />} />
             <Route path="/products/:category/:id" element={<Item />} >
               <Route path="" element={<Reviews />} />
--- client/src/components/NavBarSeller.tsx
+++ client/src/components/NavBarSeller.tsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 import "../css/navbar.css"
 
-export default function NavBar() {
+export default function NavBar(props: any) {
   return (
     <nav>
       <NavLink to="/"><img src="/logo.png" alt="logo" className="logo" /></NavLink>
       <input type="search" name="search" placeholder="Search ..." className="nav-search" />
       <ul className="nav-right">
-        <li ><NavLink to="" className="nav-item">Add Product</NavLink></li>
-        <li ><NavLink to="/login" className="nav-item">My Products</NavLink></li>
+        <li ><NavLink to="/seller/add-product" className="nav-item">Add Product</NavLink></li>
+        <li ><NavLink to="/seller" className="nav-item">My Products</NavLink></li>
         {/* Place Holder for the image of the user when he is logged in */}
       </ul>
     </nav>

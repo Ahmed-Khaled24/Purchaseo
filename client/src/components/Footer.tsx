9de2a3aca0e3fe9f5d27bd855a8448f960cdf8ba
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-16 19:43:03 +0300

    refactor: THE FOOTER

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,29 +1,89 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "../css/footer.css";
+import facebook from "../../public/facebook.png";
+import twitter from "../../public/twitter.png";
+import instagram from "../../public/instagram.png";
+import paypal from "../../public/paypal.png";
+import visa from "../../public/visa.png";
+import mastercard from "../../public/mastercard.png";
+import cash from "../../public/cash.png";
+import googleStore from "../../public/googleStore.png";
+import appStore from "../../public/appStore.png";
+
+function FooterColumn({ title, links }) {
+	return (
+		<div className="footer-column">
+			<h3 className="footer-title"> {title} </h3>
+			{links.map((link) => (
+				<Link to={"#"} className="footer-link">
+					{link}
+				</Link>
+			))}
+		</div>
+	);
+}
+function FooterColumnImage({ title, images }) {
+	return (
+		<div className="footer-column">
+			<h3 className="footer-title">{title}</h3>
+			<div className="footer-images">
+				{images.map((image) => (
+					<Link to="#">
+						<img src={image} alt="" className="footer-logo" />
+					</Link>
+				))}
+			</div>
+		</div>
+	);
+}
 
 export default function Footer() {
-  return (
-    <footer>
-      <div className="col1">
-        <a href="#" className="footer-links">About us</a>
-        <a href="#" className="footer-links">Contact us</a>
-        <a href="#" className="footer-links">Terms and Conditions</a>
-      </div>
-      <div className="col2">
-        <h3>Subscribe to Our Newsletter</h3>
-        <form>
-          <input type="email" id="subscribe" placeholder="Your E-Mail ..." />
-          <button id="subscribe-button">Subscribe</button>
-        </form>
-      </div>
-      <div className="col3">
-        <h3>Follow us on</h3>
-        <div className="icons">
-          <a href="#" className="footer-links" ><img src="/facebook.png" alt="facebook" /></a>
-          <a href="#" className="footer-links" ><img src="/twitter.png" alt="twitter" /></a>
-          <a href="#" className="footer-links" ><img src="/instagram.png" alt="instagram" /></a>
-        </div>
-      </div>
-    </footer>
-  )
+	return (
+		<footer>
+			<section className="footer-section">
+				<FooterColumn
+					title="Get to know us"
+					links={["About Purchaseo", "Careers"]}
+				/>
+				<FooterColumn
+					title="Shop with us"
+					links={[
+						"Your Account",
+						"Your Orders",
+						"Your Cart",
+						"Your addresses",
+					]}
+				/>
+				<FooterColumn
+					title="Let us help you"
+					links={["Product Support", "Purchaseo App", "Contact Us"]}
+				/>
+				<FooterColumn
+					title="Make money with us"
+					links={[
+						"Sell on Purchaseo",
+						"Become an Affiliate",
+						"Advertise Your Products",
+					]}
+				/>
+			</section>
+			<section className="footer-section logos">
+				<FooterColumnImage
+					title="Join us on"
+					images={[facebook, twitter, instagram]}
+				/>
+				<FooterColumnImage
+					title="Payment methods"
+					images={[visa, mastercard, paypal, cash]}
+				/>
+				<FooterColumnImage
+					title="download our app"
+					images={[googleStore, appStore]}
+				/>
+			</section>
+			<section className="footer-section copyright">
+				© Puchaseo 2023
+			</section>
+		</footer>
+	);
 }

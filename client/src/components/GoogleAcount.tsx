a5e98bbe4b925ba708e2dad611625475c5d6a56e
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-16 08:22:34 +0300

    feat: link google login button to api

--- client/src/components/GoogleAcount.tsx
+++ client/src/components/GoogleAcount.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import axios from 'axios'
+import keys from '../../../server/config/keys'
 
 export default function GoogleAcount(props){
 
+    async function handleGoogle(event: React.MouseEvent<HTMLButtonElement>){
+        window.location.href = `https://localhost:4000/auth/google`
+    }
+
     return(
-        <div className='google-container'>
-            <button className='google--butt'>
+        <div className='google-container' >
+            <button className='google--butt' onClick={handleGoogle} >
                 <img src = "/images/gmail.png" className='google--img'/>
-                <p className='create'>{props.name} with google</p>
+                <p className='create' >{props.name} with google</p>
             </button>
         </div>
     )

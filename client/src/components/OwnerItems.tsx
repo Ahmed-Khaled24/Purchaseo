a302dcdf1127c2fc56e289ef91f9e3d1b0389b5e
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-17 23:52:19 +0300

    feat: update owner product page
    update owner product page to handle deleting products

--- client/src/components/OwnerItems.tsx
+++ client/src/components/OwnerItems.tsx
@@ -6,29 +6,35 @@ import { RootState } from "../store/store";
 import axios from "axios";
 import { Product } from "../../../server/types/Product";
 
+const API_URL = "https://localhost:4000";
+
 export default function OwnerItems() {
 	const [items, setItems] = React.useState<Product[]>([]);
 	const user = useSelector((state: RootState) => state.user);
 	useEffect(() => {
 		axios({
+			withCredentials: true,
 			method: "get",
-			url: `http://localhost:4000/product/seller/${user.user_id}`,
+			url: `${API_URL}/product/seller/${user.user_id}`,
 		}).then((res) => {
 			setItems(res.data.data);
 		});
 	}, []);
 	return (
 		<div className="owner-items">
-			{items.length === 0 && <h1>No items to show</h1>}
-			{items.map((item: Product, index) => (
-        // TODO: Add reviews and products
+			{items.length === 0 && <h1 className="empty-products-placeholder"> No items to show </h1>}
+			{items.map((item, index) => (
+				// TODO: Add reviews and products
 				<OwnerProduct
-          price={item.price}
+					price={item.price}
 					key={index}
 					name={item.product_name}
 					rate={item.rating}
-					reviews={"haha"}
-					desc={item.description}
+					reviews={"0"}
+					image={item.images[0]}
+					sold={item.sold_quantity}
+					quantity={item.inventory}
+					id={item.product_id}
 				/>
 			))}
 		</div>
